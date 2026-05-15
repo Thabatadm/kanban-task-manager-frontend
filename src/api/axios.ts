42c2db9e91b7416a27d8c3c53a9ddf7c6549ebd7
Thatatadm <mthabatadenise@gmail.com>
2026-05-15 11:57:53 +0200

    chore: configure axios instance and vite base setup

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
@@ -11,6 +13,8 @@ api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
