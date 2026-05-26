a0ea821c595e61ca3f06267c2888ec17e9f4af86
Thatatadm <mthabatadenise@gmail.com>
2026-05-26 12:20:41 +0200

    chore: setup global routing, and core styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/login';
-import Projects from './pages/Projects'; 
-import Layout from './components/layout/Layout';
-import { useAuth } from './hooks/useAuth';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/login";
+import Projects from "./pages/Projects";
+import Layout from "./components/layout/Layout";
+import { useAuth } from "./hooks/useAuth";
+import Register from "./pages/Registrer";
+import { Profile } from "./pages/Profile";
+import { KanbanBoard } from "./pages/KanbanBoard";
+import { KanbanCalendar } from "./pages/KanbanCalendar";
 
 function App() {
   const { isLoggedIn } = useAuth();
@@ -12,22 +16,30 @@ function App() {
       {isLoggedIn ? (
         <Layout>
           <Routes>
-            <Route path="/dashboard" element={<div className="text-black dark:text-white text-2xl font-bold text-center mt-10 italic">Welcome to the Terminal Workspace</div>} />
             <Route path="/projects" element={<Projects />} />
-            
-            <Route path="/login" element={<Navigate to="/dashboard" replace />} />
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
-            
-           
-            <Route path="*" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route
+              path="/projects/:projectId/kanban"
+              element={<KanbanBoard />}
+            />
+            <Route
+              path="/projects/:projectId/calendar"
+              element={<KanbanCalendar />}
+            />
+
+            <Route
+              path="/login"
+              element={<Navigate to="/projects" replace />}
+            />
+            <Route path="/" element={<Navigate to="/projects" replace />} />
+            <Route path="*" element={<Navigate to="/projects" replace />} />
           </Routes>
         </Layout>
       ) : (
-        
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Login />} />
-          
+          <Route path="/register" element={<Register />} />
           <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       )}
