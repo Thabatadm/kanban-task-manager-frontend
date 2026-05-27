b744b7e86d93a7044ca15f948175b5bf64ac9759
Thatatadm <mthabatadenise@gmail.com>
2026-05-27 13:02:54 +0200

    Feat: The footer grid was changed to a 2-grid.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,8 +5,9 @@ export const Footer: React.FC = () => {
 
   return (
     <footer className="w-full mt-auto bg-bg-sub-dark pt-8 pb-6 border-t border-dashed border-border-grid/30 font-terminal">
-      <div className="px-6 grid grid-cols-3 gap-x-6 md:gap-x-12 divide-x divide-border-grid/30 text-[11px] text-white/70 uppercase tracking-wider text-center items-center py-2">
-        <div className="p-2 md:p-4 flex flex-col gap-2 text-center w-full">
+      <div className="px-6 grid grid-cols-2 md:grid-cols-3 gap-y-8 gap-x-6 md:gap-x-12 md:divide-x md:divide-border-grid/30 text-[11px] text-white/70 uppercase tracking-wider text-center items-center py-2">
+        
+        <div className="flex flex-col gap-2 text-center w-full">
           <span className="text-[9px] text-white/40 font-bold tracking-widest block mb-1">
             // CORE_STACK
           </span>
@@ -14,15 +15,8 @@ export const Footer: React.FC = () => {
           <span className="truncate w-full">Tailwind CSS v4</span>
           <span className="truncate w-full">RESTful API</span>
         </div>
-        <div className="p-2 md:p-4 pl-6 md:pl-12 flex flex-col gap-1 text-center w-full">
-          <span className="text-[9px] text-white/40 font-bold tracking-widest">
-            // AUTHOR_SIGNATURE
-          </span>
-          <div className="text-xs font-bold text-white/50 tracking-normal pt-0.5 break-words w-full">
-            Thabata Denise Monteiro da Silva
-          </div>
-        </div>
-        <div className="p-2 md:p-4 pl-6 md:pl-12 flex flex-col gap-3 text-center w-full">
+
+        <div className="md:pl-12 flex flex-col gap-3 text-center w-full">
           <span className="text-[9px] text-white/40 font-bold tracking-widest mb-1">
             // CONTACT_ME
           </span>
@@ -42,11 +36,20 @@ export const Footer: React.FC = () => {
           >
             LINKEDIN
           </a>
+        </div>
+          <div className="col-span-2 md:col-span-1 md:pl-12 flex flex-col gap-1 text-center w-full pt-6 border-t border-border-grid/10 md:pt-0 md:border-0">
+          <span className="text-[9px] text-white/40 font-bold tracking-widest">
+            // AUTHOR_SIGNATURE
+          </span>
+          <div className="text-xs font-bold text-white/50 tracking-normal pt-0.5 break-words w-full">
+            Thabata Denise Monteiro da Silva
+          </div>
 
-          <span className="text-[10px] text-white/40 tracking-widest pt-3 border-t border-slate-800/40 w-full mt-1">
+          <span className="text-[10px] text-white/40 tracking-widest pt-2 w-full mt-1">
             SYS_VER_1.0 // © {currentYear}
           </span>
         </div>
+
       </div>
     </footer>
   );
